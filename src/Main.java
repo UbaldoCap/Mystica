8bafafdbad16448eeab13b2dcefa22645c964a42
Ubaldo <ubaldocapalbo@gmail.com>
2023-12-06 04:00:38 +0100

    end

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,5 @@ public class Main {
     public static void main(String[] args) {
         RegistroCreature registroCreature = new RegistroCreature(new ArrayList<>());
         registroCreature.start();
-
-
-
-
-        /*CreaturaMagica warsa = new CreaturaMagica("warsa", "luce", "raggio solare", 8);
-        CreaturaMagica asdur = new CreaturaMagica("asdur", "oscurità", "verde conato", 6);
-        RegistroCreature registroCreature = new RegistroCreature(new ArrayList<>());
-        registroCreature.aggiungiCreatura(asdur);
-        registroCreature.aggiungiCreatura(warsa);
-        registroCreature.visualizzaCreature();
-        asdur.visualizzaCaratteristiche();
-        warsa.visualizzaCaratteristiche();*/
     }
 }
